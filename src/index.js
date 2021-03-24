e889e10f916cab8a7396c8289278d7fb4f51ecfb
Sandra Wazaz <sandrawazaz@gmail.com>
2021-03-23 20:38:59 -0400

    edit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// write your code here
+let url = "http://localhost:3000/ramens";
+let ramenMenuDiv = document.querySelector("div#ramen-menu")
+
+
+let getRamen = function() { 
+   fetch(url)
+   .then(response => {
+      response.json().then(data => {  
+      
+      data.forEach(ramen => {
+         let div = document.createElement('div')
+         div.innerHTML = `<img src="${ramen.image}"/>`
+         ramenMenuDiv.append(div)
+      })
+    })
+})
+.catch((err) => console.log('error', err))
+}
+
+getRamen();
